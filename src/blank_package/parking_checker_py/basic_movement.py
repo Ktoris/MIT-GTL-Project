e261a9a09d993d4dbd80f259007ca03a9cd122f1
Serioga427 <s.a.golubin@gmail.com>
2026-01-28 13:07:00 +0300

    updated basic_movement

--- src/blank_package/parking_checker_py/basic_movement.py
+++ src/blank_package/parking_checker_py/basic_movement.py
@@ -1,12 +1,14 @@
-import rospy
+import rclpy
+from rclpy.node import Node
 from geometry_msgs.msg import Twist
 import time
 
 
-class ScannerController:
+class ScannerController(Node):
     def __init__(self):
-        rospy.init_node("scanner_controller")
-        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+        super().__init__("scanner_controller")
+
+        self.pub = self.create_publisher(Twist, "/cmd_vel", 10)
 
         # ===== MOTION PARAMETERS (TUNE THESE) =====
         self.angular_speed = 1.2     # rad/s
@@ -15,8 +17,9 @@ class ScannerController:
         self.center_time = 1.2
         self.back_time = 0.6
 
-        rospy.sleep(1.0)
-        rospy.loginfo("Scanner Controller started")
+        time.sleep(1.0)
+        self.get_logger().info("Scanner Controller started")
+
         self.scan_loop()
 
     def send_cmd(self, linear=0.0, angular=0.0, duration=1.0):
@@ -25,14 +28,4 @@ class ScannerController:
         twist.angular.z = angular
 
         start = time.time()
-        rate = rospy.Rate(20)
-
-        while time.time() - start < duration and not rospy.is_shutdown():
-            self.pub.publish(twist)
-            rate.sleep()
-
-        self.stop()
-
-    def stop(self):
-        self.pub.publish(Twist())
-        rospy.sleep
+        rate_hz = 20
